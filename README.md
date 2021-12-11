fb1ea43e17bb5529aefe408b46522081afed5cc6
luck_baby <67427627+ranyong1997@users.noreply.github.com>
2021-12-12 00:16:36 +0800

    add newly increased (新增日志系统)

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Api_Automator
+
+## 实现python+pytest+allure+requests的接口自动化
+
+> V1.0.0
+> Data：2021.12.11
+> 初始化仓库
---
+++ common/logger.py
@@ -0,0 +1,55 @@
+import os
+import datetime
+from loguru import logger
+
+
+class Logings:
+    __instance = None
+    # 文件名称，按天创建
+    DATE = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+
+    # 项目路径下创建log目录保存日志文件
+    logpath = os.path.join(os.path.dirname(os.getcwd()), "logs")  # 拼接指定路径
+    # 判断目录是否存在，不存在则创建新的目录
+    if not os.path.exists(logpath):
+        os.mkdir(logpath)
+
+    logger.add('%s/%s.log' % (logpath, DATE),  # 指定文件
+               format="{time:YYYY-MM-DD HH:mm:ss}  | {level}> {elapsed}  | {message}",
+               encoding='utf-8',
+               retention='1 days',  # 设置历史保留时长
+               backtrace=True,  # 回溯
+               diagnose=True,  # 诊断
+               enqueue=True,  # 异步写入
+               # rotation="5kb",  # 切割，设置文件大小，rotation="12:00"，rotation="1 week"
+               # filter="my_module"  # 过滤模块
+               # compression="zip"   # 文件压缩
+               )
+
+    def __new__(cls, *args, **kwargs):
+        if not cls.__instance:
+            cls.__instance = super(Logings, cls).__new__(cls, *args, **kwargs)
+        return cls.__instance
+
+    def info(self, msg, *args, **kwargs):
+        return logger.info(msg, *args, **kwargs)
+
+    def debug(self, msg, *args, **kwargs):
+        return logger.debug(msg, *args, **kwargs)
+
+    def warning(self, msg, *args, **kwargs):
+        return logger.warning(msg, *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        return logger.error(msg, *args, **kwargs)
+
+    # def exception(self, msg, *args, exc_info=True, **kwargs):
+    #     return logger.exception(msg, *args, exc_info=True, **kwargs)
+
+
+if __name__ == '__main__':
+    logs = Logings()
+    logs.info("测试")
+    logs.debug("测试")
+    logs.warning("测试")
+    logs.error("测试")
